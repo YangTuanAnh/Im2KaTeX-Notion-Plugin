d30368b6b7f4d5331c9fe263bc8de606e55a9c32
YangTuanAnh <yangtuananh2003@gmail.com>
2023-06-04 20:17:11 +0700

    change: main function

--- app.py
+++ app.py
@@ -66,25 +66,28 @@ def redirect_button(url: str, text: str= None, color="#FD504D"):
     unsafe_allow_html=True
     )
 
-        
-st.title('Im2KaTeX Notion Plugin')
-url = "https://api.notion.com/v1/oauth/authorize?client_id=abf6a6f6-1369-488e-8f23-18ee7ad157b2&response_type=code&owner=user&redirect_uri=https%3A%2F%2Fyangtuananh-im2katex-notion-plugin-app-qucmyk.streamlit.app%2F"
+def main():
+    st.title('Im2KaTeX Notion Plugin')
+    url = "https://api.notion.com/v1/oauth/authorize?client_id=abf6a6f6-1369-488e-8f23-18ee7ad157b2&response_type=code&owner=user&redirect_uri=https%3A%2F%2Fyangtuananh-im2katex-notion-plugin-app-qucmyk.streamlit.app%2F"
 
-redirect_button(url,"Authorize Integration to Notion account")
+    redirect_button(url,"Authorize Integration to Notion account")
 
-page_id = st.text_input("Notion Page ID")
-st.write("https://www.notion.so/Personal-Home- **6b241a4133ba4db18f5bd9fbc76e6856** 👈 Example Page ID")
-if st.checkbox('Upload with Camera'):
-    picture = st.camera_input("Take a picture")
-else:
-    picture = st.file_uploader("Choose a file", type=['png', 'jpg'])
+    page_id = st.text_input("Notion Page ID")
+    st.write("https://www.notion.so/Personal-Home- **6b241a4133ba4db18f5bd9fbc76e6856** 👈 Example Page ID")
+    if st.checkbox('Upload with Camera'):
+        picture = st.camera_input("Take a picture")
+    else:
+        picture = st.file_uploader("Choose a file", type=['png', 'jpg'])
 
-if picture is not None:
-    st.image(picture)
-    st.subheader("Prediction")
-    pred = getPrediction(picture)
-    #pred = "\sin{x}^2 + \cos{x}^2 = 1"
-    #output = st.text_area("Edit KaTeX here", pred)
-    st.latex(st.text_area("Edit KaTeX here", pred))
-    if st.button('Upload to Notion page'):
-        uploadKaTeX(page_id, pred)
+    if picture is not None:
+        st.image(picture)
+        st.subheader("Prediction")
+        pred = getPrediction(picture)
+        #pred = "\sin{x}^2 + \cos{x}^2 = 1"
+        #output = st.text_area("Edit KaTeX here", pred)
+        st.latex(st.text_area("Edit KaTeX here", pred))
+        if st.button('Upload to Notion page'):
+            uploadKaTeX(page_id, pred)
+            
+if __name__ == "__main__":
+    main()
